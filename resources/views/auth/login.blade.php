2e8a91e931fe7d991de61532f8118cf6fefdd340
Bruno Rizzo <bruno25.rs@gmail.com>
2022-09-17 20:45:55 -0300

    new login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,86 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory | Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossorigin="anonymous" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <link rel="stylesheet" href="{{asset('/vendor/bootstrap/dist/css/bootstrap.min.css')}}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    <link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
+    <!-- <link rel="stylesheet" href="../vendor/themify-icons/themify-icons.css"> -->
+    <link rel="stylesheet" href="{{asset('/assets/css/bootstrap-override.css')}}">
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
+</head>
+
+<body>
+<section class="container h-100">
+    <div class="row justify-content-sm-center h-100 align-items-center">
+        <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-7 col-sm-8">
+            <div class="card shadow-lg">
+                <div class="card-body p-4">
+                    <h1 class="fs-4 text-center fw-bold mb-4">Login</h1>
+                    <h1 class="fs-6 mb-3">Please enter your email and password to log in.</h1>
+
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                    <form action="{{route('login')}}" method="POST" aria-label="abdul" data-id="abdul" class="needs-validation" novalidate=""
+                        autocomplete="off">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="mb-2 text-muted" for="email">E-Mail Address</label>
+                            <div class="input-group input-group-join mb-3">
+                                <input id="email" type="email" placeholder="Enter Email" class="form-control"
+                                    name="email" required autofocus>
+                                    <span class="input-group-text rounded-end">&nbsp<i class="fa fa-envelope"></i>&nbsp</span>
+                                <div class="invalid-feedback">
+                                    Email is invalid
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <div class="mb-2 w-100">
+                                <label class="text-muted" for="password">Password</label>
+                           </div>
+                            <div class="input-group input-group-join mb-3">
+                                <input type="password" class="form-control" placeholder="Your password" required name="password">
+                                <span class="input-group-text rounded-end password cursor-pointer">&nbsp<i class="fa fa-eye"></i>&nbsp</span>
+                                <div class="invalid-feedback">
+                                    Password required
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="align-items-center">
+                            <button type="submit" class="btn btn-primary ps-5 pe-5 mt-">
+                                Login
+                            </button>
+                            
+                        </div>
+
+                        
+                    </form>
+
+                    <div class="text-center mt-4">
+                        <a href="{{url('/')}}">Página Inicial</a>
+                    </div>
+                    
+               </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <div class="text-center mt-5 text-muted">
+                Copyright &copy; 2022
             </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+</section>
+<script src="{{asset('/assets/js/login.js')}}"></script>
+</body>
+</html>
